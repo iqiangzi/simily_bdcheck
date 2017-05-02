d5229680d99320b4121ad43bcd9d7f7185233045
Cheery2017 <1933546618@qq.com>
2017-05-02 15:08:33 +0800

    getPath

---
+++ util/toolUtils/getPath.py
@@ -0,0 +1,30 @@
+import os
+
+class GetPath(object):
+    #获取所需文件在当前电脑中的绝对路径
+    def getAbsoluteFilePath(self,file_name,file_partpath):
+        """
+        :param file_name: 文件全称.例："testvb.exe"
+        :param file_partpath: testData下的文件路径.例：r"massProduceUploadApps\testvb.exe"
+        :return:
+        """
+        path = os.path.abspath(file_name)
+        path_dir = path.split("simily_bdcheck")
+        absolutepath = str(path_dir[0])+r"simily_bdcheck\testData\%s"%(file_partpath)
+        return absolutepath
+
+    def getAbsoluteDirPath(self,file_partpath):
+        """
+        获取当前目录在本机的绝对路径
+        :param file_partpath: testData下的文件路径.例：r"massProduceUploadApps\testvb.exe"
+        :return:
+        """
+        path = os.path.abspath(file_partpath)
+        path_dir = path.split("simily_shuobo")
+        absolutepath = str(path_dir[0])+r"simily_bdcheck\testData\%s"%(file_partpath)
+        #print( "全路径为：",str(absolutepath))
+        return absolutepath
+
+if __name__ == "__main__":
+    print(GetPath().getAbsoluteDirPath("downloadFiles"))
+    GetPath().getAbsoluteFilePath("upload_file_wrongtitle.xlsx",r"massProduceTestData\upload_file_wrongtitle.xlsx")
