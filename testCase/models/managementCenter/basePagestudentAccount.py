04ed06c709481541d0d323f2ad9b7eb2b284a1c8
jiulinghouxiaoyan <1406487226@qq.com>
2017-05-09 16:36:17 +0800

    管理中心-学生管理测试用例

---
+++ testCase/models/managementCenter/basePagestudentAccount.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/3/13 13:25
+# @Author  : Nxy
+# @Site    :
+# @File    : BasePage.py
+# @Software: PyCharm
+from testCase.models import myUnitFirefox
+from selenium import webdriver
+import unittest
+from time import sleep
+from selenium.webdriver.common.by import By
+class BasePage(object):
+    '''
+    页面基础类，用于账户信息页面的继承
+
+    '''
+    #账户信息URL
+    burl="http://check.test.wanfangdata.com.cn/bd/Student/#"
+
+    def __init__(self,selenium_driver,base_url= burl,parent=None):
+        self.base_url = base_url
+        self.driver = selenium_driver
+        self.timeout = 30
+        self.parent = parent
+
+    def _open(self,url):
+        url = self.base_url +url
+        self.driver.get(url)
+
+
+    def find_element(self,*loc):
+        return self.driver.find_element(*loc)
+
+    def find_elements(self,*loc):
+        return self.driver.find_elements(*loc)
+
+    def open(self,url):
+        self._open(url)
+
+    def script(self,src):
+        return self.driver.execute_script(src)
+    #处理弹出窗口，注意是确认窗口
+    def confirm_window(self):
+        return self.driver.switch_to.alert().text
+    #frame转换
+    def user_login_switch(self,id):
+        self.driver.switch_to.frame(id)
+    #跳到最外层窗口
+    def user_switch_default(self):
+        self.driver.switch_to.default_content()
+
+class UserVer(BasePage):
+    #登录各个元素定位
+    login_username_loc=(By.ID,"userid")
+    login_password_loc=(By.ID,"password")
+    login_button_loc=(By.XPATH,"/html/body/form/div[5]/div/div[1]/div[6]/input")
+
+    def userLogin(self,username,password):
+        '''
+        用户登录
+        :param username: 用户名
+        :param password: 用户登录密码
+        :return: 无返回值
+        '''
+        '''获取的用户名密码登录'''
+        self.open("")
+        self.login_username(username)
+        self.login_password(password)
+        self.login_button()
+        sleep(5)
+
+    #登录用户名
+    def login_username(self,username):
+        self.find_element(*self.login_username_loc).clear()
+        self.find_element(*self.login_username_loc).send_keys(username)
+    #登录密码
+    def login_password(self,password):
+        self.find_element(*self.login_password_loc).clear()
+        self.find_element(*self.login_password_loc).send_keys(password)
+    #登录按钮
+    def login_button(self):
+        self.find_element(*self.login_button_loc).click()
+
+if __name__=='__main__':
+   unittest.main()
--- testRun/run_managementCenter_studentAccount.py
+++ testRun/run_managementCenter_studentAccount.py
@@ -5,17 +5,120 @@
 # @Site    : 
 # @File    : run_studentAccount.py
 # @Software: PyCharm
-from selenium import  webdriver
+from selenium import webdriver
 import unittest
 from HTMLTestRunner import HTMLTestRunner
-import time
+from time import sleep
 from testCase.models import myUnitChrome
 from testCase.models import myUnitFirefox
+from testCase.models.managementCenter.basePagestudentAccount import UserVer
+from testCase.models.managementCenter.studentAccount import StudentAccount
+from testResult.getResultImage import getResultImage
+import os
 
 class RunStudentAccount(myUnitChrome.UnitChrome):
-    def test_query_run(self):
-        pass
-    def test_export_run(self):
-        pass
-    def test_studentAccountsInfo_run(self):
-        pass
+
+    def test_downloadtemplate_run(self):
+        '''下载学生账户模板成功'''
+        #登录系统
+        login=UserVer(self.driver)
+        login.userLogin("collegeuser","f")
+        sleep(2)
+
+        stu=StudentAccount(self.driver)
+        stu.in_batchstudent()
+        sleep(2)
+
+        flag=stu.verifyExist()
+
+        if flag is True:
+            stu.renameFileName()
+            stu.downloadaccount_template()
+            sleep(5)
+            print("模板已存在，模板下载成功")
+
+        else:
+            #单击导出按钮
+            stu.downloadaccount_template()
+            sleep(5)
+            print("模板下载成功")
+
+        sleep(5)
+        #获取页面截图
+        imagetest = getResultImage()
+        imagetest.insert_image(self.driver,"download_studentaccount_template.jpg")
+
+    def test_chooseexistBatch_run(self):
+        '''批量生成学生账户-分配检测篇数超过剩余检测篇数'''
+        #登录系统
+        login=UserVer(self.driver)
+        login.userLogin("collegeuser","f")
+        sleep(2)
+
+        stu=StudentAccount(self.driver)
+        stu.in_batchstudent()
+        sleep(2)
+        stu.choose_existBatch()
+        stu.expirydate()
+        sleep(2)
+        a=stu.checkcount_surplus()+1
+        print(a)
+        stu.checkcount_input(checkcount=a)
+        sleep(2)
+        #获取页面截图
+        imagetest = getResultImage()
+        imagetest.insert_image(self.driver,"chooseexistBatch.jpg")
+    def test_choosenewBatch_run(self):
+        '''批量生成学生账户-新建批次'''
+        #登录系统
+        login=UserVer(self.driver)
+        login.userLogin("collegeuser","f")
+        sleep(2)
+
+        stu=StudentAccount(self.driver)
+        stu.in_batchstudent()
+        sleep(2)
+        stu.choose_newBatch(batchName="newbatch1")
+        sleep(2)
+
+        #获取页面截图
+        imagetest = getResultImage()
+        imagetest.insert_image(self.driver,"choosenewBatch.jpg")
+    def test_upload_run(self):
+        #登录系统
+        login=UserVer(self.driver)
+        login.userLogin("collegeuser","f")
+        sleep(2)
+
+        stu=StudentAccount(self.driver)
+        stu.in_batchstudent()
+        sleep(2)
+        stu.choose_existBatch()
+        stu.expirydate()
+        sleep(2)
+
+        stu.checkcount_input(checkcount=1)
+        sleep(2)
+        print("基本条件输入成功")
+        stu.upload_stuaccount()
+        sleep(3)
+
+        #调用autoIt生成的上传文件应用
+        stu.uploadFile_para("chrome",stu.getFilePath("111.xlsx"))
+        sleep(5)
+        #msg=stu.getWinAlert()
+        #print(msg)
+        self.assertEqual(stu.uploaderror_remind(),"全部学生账号信息不规范或该账号已存在")
+        print("上传成功")
+
+
+        #获取页面截图
+        imagetest = getResultImage()
+        imagetest.insert_image(self.driver,"uploadPaperExist.jpg")
+
+
+
+
+
+
+
