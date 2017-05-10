5e0b6576ba964179fc33b74afa9c879cb0b3c2f7
Cheery2017 <1933546618@qq.com>
2017-05-10 13:07:53 +0800

    我的检测 -任务列表

--- testCase/models/myTest/taskList.py
+++ testCase/models/myTest/taskList.py
@@ -27,6 +27,7 @@ class TaskList(BasePage):
     search_button_loc = (By.XPATH,".//*[@id='searchTaskForm']/div/input")
     # 任务名称列表：总结出查询结果中的任务数目
     task_list_loc = (By.XPATH, "html/body/div[4]/div[3]/table/tbody/tr/td[2]")
+    author_link_loc=(By.XPATH, "html/body/div[4]/div[2]/table/tbody/tr")
     # 分页定位
     paging_select_loc = (By.ID, "sel")
     # 首页，上一页
@@ -67,11 +68,39 @@ class TaskList(BasePage):
     task_search_loc = (By.XPATH, ".//*[@id='searchTaskItemForm']/div[2]/p[2]/input")
     #选择状态
     #select_status_choose = (By.XPATH,".//*[@id='searchTaskItemForm']/div[2]/p[2]/select")
-    #task_result_status = (By.XPATH, "html/body/div[4]/div[2]/table/tbody/tr[2]/td[7]")
+    task_result_status = (By.XPATH, "html/body/div[4]/div[2]/table/tbody/tr[2]/td[7]")
     # 下载报告 按钮
-    down_report_btn = (By.ID,"downLoadReport")
+    down_report_btn = (By.ID, "downLoadReport")
     # 勾选复选框
-    select_btn=(By.ID,"allchecked")
+    select_btn=(By.ID, "allchecked")
+    # 转移按钮
+    transfor_btn_loc=(By.ID, "moveToOtherTask")
+    # 第一个任务
+    first_task_loc=(By.XPATH, ".//*[@id='moveToOtherTaskList']/li[1]")
+    # 第一行复选框
+    first_select_loc=(By.XPATH, "html/body/div[4]/div[2]/table/tbody/tr[2]/td[2]/input")
+    # 标记为按钮
+    mark_btn_loc=(By.ID, "markPaper")
+    # 标记为问题论文
+    mark_problem_btn=(By.ID, "markTroublePaperBtn")
+    # 取消标记为问题论文
+    concel_mark_paper=(By.ID, "unMarkTroublePaperBtn")
+    # 问题论文 数目
+    mark_problem_num=(By.XPATH, ".//*[@id='searchTaskItemForm']/div[1]/span/span[2]")
+    # 检测时间控件
+    test_time_order=(By.ID, "dateSortBy")
+    # V2.0相似比排序控件
+    similar_radio_order=(By.XPATH, ".//*[@id='copyprecentSortBy'][2]")
+    # V1.0相似比排序控件
+    similar_radio_order1=(By.XPATH, ".//*[@id='copyprecentSortBy'][1]")
+    # 简明报告
+    report1_loc=(By.XPATH, "html/body/div[4]/div[2]/table/tbody/tr[2]/td[9]/label[1]")
+    # 详细报告
+    report2_loc=(By.XPATH, "html/body/div[4]/div[2]/table/tbody/tr[2]/td[9]/label[2]")
+    # 全文报告
+    report3_loc=(By.XPATH, "html/body/div[4]/div[2]/table/tbody/tr[2]/td[9]/label[3]")
+    # 在线报告
+    mark_loc=(By.ID, "Simple")
 #------------------------------------------------------------------------------------------------------------------
     def list_search(self,task_name):
         '''
@@ -246,7 +275,7 @@ class TaskList(BasePage):
         sleep(1)
         # 定位第一行日期
         date=self.driver.find_element_by_xpath("html/body/div[4]/div[2]/table/tbody/tr[2]/td[6]").text
-        # 定位 状态
+        # 定位 状态 test_status_loc
         result_status=self.find_element(*self.task_result_status).text
         # return date,status
         if result_status == status:
@@ -257,6 +286,12 @@ class TaskList(BasePage):
         self.find_element(*self.select_btn).click()
         self.find_element(*self.down_report_btn).click()
         #pass
+
+    # 判断目录中是否存在同名文件 有的话返回True
+    def downVerify(self):
+        ab_path = GetPath().getAbsoluteFilePath("ncy(V1.0简明版报告).zip",r"downloadFiles\ncy(V1.0简明版报告).zip")
+        flag = os.path.exists(ab_path)
+        return flag
 
     #判断文件夹是否为空，不为空则重命名同名文件，以保证新下载的文件的唯一性。为空则直接下载
     def renameFileName(self):
@@ -270,20 +305,182 @@ class TaskList(BasePage):
             #pass
             print("文件夹为空")
 
-    #判断是否下载到指定文件夹,返回bool类型的True或False
-    def downVerify(self):
-        ab_path = GetPath().getAbsoluteFilePath("ncy(V1.0简明版报告).zip",r"downloadFiles\ncy(V1.0简明版报告).zip")
+    # 输入篇名，点击搜索
+    def title_search(self,title):
+        self.find_element(*self.title_input_loc).send_keys(title)
+        self.find_element(*self.search_btn_loc).click()
+    # 转移任务
+    def transferTask(self):
+        # 点击 下拉框
+        self.find_element(*self.transfor_btn_loc).click()
+        # 选择第一个项目
+        self.find_element(*self.first_task_loc).click()
+        time.sleep(2)
+    # 搜索之后第一个行的人物名
+    def taskName(self):
+        task = self.find_element(*self.first_task_loc).text
+        return task
+
+    #返回任务列表
+    def returnTask(self,task,verify_title):
+        # 点击 我的检测
+        self.driver.find_element_by_xpath(".//*[@id='searchTaskItemForm']/div[1]/a").click()
+        # 任务名称 检索
+        self.find_element(*self.task_name_loc).send_keys(task)
+        self.find_element(*self.search_button_loc).click()
+        time.sleep(1)
+        # 进入名称链接
+        self.find_element(*self.taskName_Link_loc).click()
+        time.sleep(1)
+        # 输入篇名
+        self.find_element(*self.title_input_loc).send_keys(verify_title)
+        # 点击查询按钮
+        self.find_element(*self.search_btn_loc).click()
+        sleep(1)
+        title=self.find_element(*self.firstL_title_loc).text
+        return title
+    # 标记为问题论文
+    def markProblem(self):
+        # self.find_element(*self.first_select_loc).click()
+        self.find_element(*self.mark_btn_loc).click()
+        self.find_element(*self.mark_problem_btn).click()
+    # 取消标记为问题论文
+    def concelMark(self):
+        self.find_element(*self.mark_btn_loc).click()
+        self.find_element(*self.concel_mark_paper).click()
+    # 显示问题论文的数目
+    def paperNum(self):
+        problem_num=self.find_element(*self.mark_problem_num).text
+        return problem_num
+        # pass
+    def paperNameLink(self):
+        title = self.find_element(*self.firstL_title_loc).text
+        author = self.find_element(*self.firstL_author_loc).text
+        newTitle = title+'+'+author+'.pdf'
+        self.find_element(*self.firstL_title_loc).click()
+        return newTitle
+
+    # 判断目录中是否存在同名文件 有的话返回True
+    def downVerify1(self,newTitle):
+        ab_path = GetPath().getAbsoluteFilePath(newTitle,r"downloadFiles\%s" % newTitle)
         flag = os.path.exists(ab_path)
         return flag
 
-    def markProblem(self):
-        pass
-    def cancelMarkPaper(self):
-        pass
-    def paperNameLink(self):
-        pass
-    def authorLink(self):
-        pass
+    # 判断文件夹是否为空，不为空则重命名同名文件，以保证新下载的文件的唯一性。为空则直接下载
+    def renameFileName1(self,newTitle):
+        ab_path = GetPath().getAbsoluteFilePath(newTitle,r"downloadFiles\%s" % newTitle)
+        ab_path_rename = GetPath().getAbsoluteFilePath("%s" % newTitle,r"downloadFiles")
+        #获取当前时间
+        name=time.strftime("%Y-%m-%d %H_%M_%S")
+        if os.path.exists(ab_path):
+            os.rename(ab_path,ab_path_rename+"\%s.pdf"%(name))
+            print("文件夹不为空")
+        else:
+            #pass
+            print("文件夹为空")
 
+    def authorLink(self):
+        # 点击第一行的作者
+        self.find_element(*self.firstL_author_loc).click()
+        auther_name = self.find_element(*self.firstL_author_loc).text
+        list_num=len(self.find_elements(*self.author_link_loc))
+        #print(list_num)
+        i=2
+        for num in range(1,list_num):
+            try:
+                #print(i)
+                auther=self.driver.find_element_by_xpath("html/body/div[4]/div[2]/table/tbody/tr[%s]/td[5]/a" % i).text
+                #print("text",auther)
+                #print(auther_name)
+                if auther_name == auther:
+                    print("有")
+                    if i==list_num:
+                        return True
+                    i=i+1
+                else:
+                    print("查询问题")
+            except Exception as msg:
+                print(msg)
     def viewOnlineReports(self):
         pass
+
+    # 层层返回
+    def alert(self):
+        return super(TaskList,self).confirm_broserAlert()
+
+    def timeOrder(self):
+        date1 =self.driver.find_element_by_xpath("html/body/div[4]/div[2]/table/tbody/tr[2]/td[6]").text
+        new_time = time.strptime(date1,"%Y-%m-%d %H:%M")
+        self.find_element(*self.test_time_order).click()
+        date2 = self.driver.find_element_by_xpath("html/body/div[4]/div[2]/table/tbody/tr[2]/td[6]").text
+        old_time = time.strptime(date2,"%Y-%m-%d %H:%M")
+        if new_time >= old_time:
+            return True
+    # 对应V1.0
+    def similarOrder(self):
+        self.find_element(*self.similar_radio_order).click()
+        str = self.find_element(*self.firstL_similar_loc).text
+        a=str.split("|")
+        # 1对应1.0
+        one_similar = float(a[1])
+        print(one_similar)
+        self.find_element(*self.similar_radio_order).click()
+        str2 = self.find_element(*self.firstL_similar_loc).text
+        b = str2.split("|")
+        two_similar = float(b[1])
+        print(two_similar)
+        if one_similar >= two_similar:
+            return True
+
+    # 对应V2.0
+    def similarOrder1(self):
+        self.find_element(*self.similar_radio_order1).click()
+        str = self.find_element(*self.firstL_similar_loc).text
+        a=str.split("|")
+        # 1对应1.0
+        one_similar = float(a[0])
+        # print(one_similar)
+        self.find_element(*self.similar_radio_order1).click()
+        str2 = self.find_element(*self.firstL_similar_loc).text
+        b = str2.split("|")
+        two_similar = float(b[0])
+        # print(two_similar)
+        if one_similar >= two_similar:
+            return True
+
+    # 下载报告 1 2 3 对应 简明报告 详细报告 全文报告
+    def downReport(self,label_num):
+        return self.driver.find_element_by_xpath("html/body/div[4]/div[2]/table/tbody/tr[2]/td[9]/label[%s]" % label_num).text
+
+    def loadReport(self):
+        title = self.find_element(*self.firstL_title_loc).text
+        author = self.find_element(*self.firstL_author_loc).text
+        newTitle = title+'+'+author
+        return newTitle
+
+    def loadReport1(self):
+        title = self.find_element(*self.firstL_title_loc).text
+        newTitle ='《'+ title+'》 '
+        return newTitle
+
+    def onlineReport(self,num):
+        # 转换句柄
+        now_handle = self.driver.current_window_handle
+        self.driver.find_element_by_xpath("html/body/div[4]/div[2]/table/tbody/tr[2]/td[10]/a[%s]" % num).click()
+        # 获取所有的句柄
+        all_handles=self.driver.window_handles
+        for handle in all_handles:
+            if handle != now_handle:
+                self.driver.switch_to.window(handle)
+                flag = self.is_element_visible(self.mark_loc)
+                return flag
+
+
+
+
+
+
+
+
+
+
