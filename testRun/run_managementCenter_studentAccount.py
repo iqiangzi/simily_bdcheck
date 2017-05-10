86483da6377da5e7a0e6cd5bf7c458951f0fe744
jiulinghouxiaoyan <1406487226@qq.com>
2017-05-10 13:13:05 +0800

    管理中心-学生账户测试用例

--- testRun/run_managementCenter_studentAccount.py
+++ testRun/run_managementCenter_studentAccount.py
@@ -42,13 +42,13 @@ class RunStudentAccount(myUnitChrome.UnitChrome):
             stu.downloadaccount_template()
             sleep(5)
             print("模板下载成功")
-
+        print("下载学生账户信息模板测试成功")
         sleep(5)
         #获取页面截图
         imagetest = getResultImage()
         imagetest.insert_image(self.driver,"download_studentaccount_template.jpg")
 
-    def test_chooseexistBatch_run(self):
+    def test_checkCount_more_run(self):
         '''批量生成学生账户-分配检测篇数超过剩余检测篇数'''
         #登录系统
         login=UserVer(self.driver)
@@ -65,9 +65,21 @@ class RunStudentAccount(myUnitChrome.UnitChrome):
         print(a)
         stu.checkcount_input(checkcount=a)
         sleep(2)
+        stu.upload_stuaccount()
+        sleep(3)
+
+        #调用autoIt生成的上传文件应用
+        stu.uploadFile_para("chrome",stu.getFilePath("学生账户1.xlsx"))
+        sleep(5)
+        errorMessage="为1个学生账户分配"+str(a)+"篇检测数量，共需分配"+str(a)+"篇，您的可分配篇数不足，请充值后，再导入学生账户表格"
+        #msg=stu.getWinAlert()
+
+        self.assertEqual(stu.uploaderror_remind(),errorMessage)
+        print("批量生成学生账户-分配检测篇数超过剩余检测篇数测试成功")
         #获取页面截图
         imagetest = getResultImage()
         imagetest.insert_image(self.driver,"chooseexistBatch.jpg")
+
     def test_choosenewBatch_run(self):
         '''批量生成学生账户-新建批次'''
         #登录系统
@@ -84,7 +96,8 @@ class RunStudentAccount(myUnitChrome.UnitChrome):
         #获取页面截图
         imagetest = getResultImage()
         imagetest.insert_image(self.driver,"choosenewBatch.jpg")
-    def test_upload_run(self):
+    def test_upload_TooBig_run(self):
+        '''上传的文件超过5M'''
         #登录系统
         login=UserVer(self.driver)
         login.userLogin("collegeuser","f")
@@ -99,23 +112,331 @@ class RunStudentAccount(myUnitChrome.UnitChrome):
 
         stu.checkcount_input(checkcount=1)
         sleep(2)
-        print("基本条件输入成功")
+        stu.upload_stuaccount()
+        sleep(3)
+
+        #调用autoIt生成的上传文件应用
+        stu.uploadFile_para("chrome",stu.getFilePath("Patent_Application_Info.xlsx"))
+        sleep(5)
+        msg=stu.uploaderror_remind()
+        #print(msg)
+        self.assertEqual(stu.uploaderror_remind(),"上传失败，文件过大!",msg)
+        print("上传文件过大测试成功！")
+
+        #获取页面截图
+        imagetest = getResultImage()
+        imagetest.insert_image(self.driver,"upload_TooBig.jpg")
+    def test_upload_empty_run(self):
+        '''上传的文件为空'''
+        #登录系统
+        login=UserVer(self.driver)
+        login.userLogin("collegeuser","f")
+        sleep(2)
+
+        stu=StudentAccount(self.driver)
+        stu.in_batchstudent()
+        sleep(2)
+        stu.choose_existBatch()
+        stu.expirydate()
+        sleep(2)
+
+        stu.checkcount_input(checkcount=1)
+        sleep(2)
+        stu.upload_stuaccount()
+        sleep(3)
+
+        #调用autoIt生成的上传文件应用
+        stu.uploadFile_para("chrome",stu.getFilePath("内容为空.xlsx"))
+        sleep(5)
+        msg=stu.uploaderror_remind()
+        #print(msg)
+        self.assertEqual(stu.uploaderror_remind(),"内容为空.xlsx文件解析失败",msg)
+        print("上传文件内容为空测试成功！")
+
+        #获取页面截图
+        imagetest = getResultImage()
+        imagetest.insert_image(self.driver,"upload_empty.jpg")
+
+    def test_upload_toomany_run(self):
+        '''上传文件账户超过200条'''
+        #登录系统
+        login=UserVer(self.driver)
+        login.userLogin("collegeuser","f")
+        sleep(2)
+
+        stu=StudentAccount(self.driver)
+        stu.in_batchstudent()
+        sleep(2)
+        stu.choose_existBatch()
+        stu.expirydate()
+        sleep(2)
+
+        stu.checkcount_input(checkcount=1)
+        sleep(2)
+        stu.upload_stuaccount()
+        sleep(3)
+
+        #调用autoIt生成的上传文件应用
+        stu.uploadFile_para("chrome",stu.getFilePath("学生账户超过200条.xlsx"))
+        sleep(5)
+        msg=stu.uploaderror_remind()
+        #print(msg)
+        self.assertEqual(stu.uploaderror_remind(),"学生账号数量超过最大限制200条",msg)
+        print("上传文件账户超过限制测试成功！")
+
+        #获取页面截图
+        imagetest = getResultImage()
+        imagetest.insert_image(self.driver,"upload_toomany.jpg")
+
+    '''
+    #备注：目前插件版本太低，不是规定的格式不会进行任何操作
+    def test_upload_formaterror_run(self):
+        #上传文件格式不是xls或xlsx
+        #登录系统
+        login=UserVer(self.driver)
+        login.userLogin("collegeuser","f")
+        sleep(2)
+
+        stu=StudentAccount(self.driver)
+        stu.in_batchstudent()
+        sleep(2)
+        stu.choose_existBatch()
+        stu.expirydate()
+        sleep(2)
+
+        stu.checkcount_input(checkcount=1)
+        sleep(2)
+        stu.upload_stuaccount()
+        sleep(3)
+
+        #调用autoIt生成的上传文件应用
+        stu.uploadFile_para("chrome",stu.getFilePath("general.txt"))
+        sleep(5)
+        msg=stu.getWinAlert()
+        #print(msg)
+        self.assertEqual(stu.uploaderror_remind(),"内容为空.xlsx文件解析失败",msg)
+        print("上传文件类型不是规定类型测试成功！")
+
+        #获取页面截图
+        imagetest = getResultImage()
+        imagetest.insert_image(self.driver,"upload_formaterror.jpg")
+'''
+
+    def test_upload_format_xls_run(self):
+        '''上传的文件格式为xls'''
+        #登录系统
+        login=UserVer(self.driver)
+        login.userLogin("collegeuser","f")
+        sleep(2)
+
+        stu=StudentAccount(self.driver)
+        stu.in_batchstudent()
+        sleep(2)
+        stu.choose_existBatch()
+        stu.expirydate()
+        sleep(2)
+
+        stu.checkcount_input(checkcount=1)
+        sleep(2)
+        stu.upload_stuaccount()
+        sleep(3)
+
+        #调用autoIt生成的上传文件应用
+        stu.uploadFile_para("chrome",stu.getFilePath("学生账户.xls"))
+        sleep(5)
+        msg=stu.uploaderror_remind()
+        #print(msg)
+        self.assertEqual(stu.uploaderror_remind(),"全部学生账号添加成功",msg)
+        print("上传文件格式为.xls测试成功！")
+
+        #获取页面截图
+        imagetest = getResultImage()
+        imagetest.insert_image(self.driver,"upload_format_xls.jpg")
+    def test_upload_format_xlsx_run(self):
+        '''上传的文件格式为xlsx'''
+        #登录系统
+        login=UserVer(self.driver)
+        login.userLogin("collegeuser","f")
+        sleep(2)
+
+        stu=StudentAccount(self.driver)
+        stu.in_batchstudent()
+        sleep(2)
+        stu.choose_existBatch()
+        stu.expirydate()
+        sleep(2)
+
+        stu.checkcount_input(checkcount=1)
+        sleep(2)
+        stu.upload_stuaccount()
+        sleep(3)
+
+        #调用autoIt生成的上传文件应用
+        stu.uploadFile_para("chrome",stu.getFilePath("学生账户1.xlsx"))
+        sleep(5)
+        msg=stu.uploaderror_remind()
+        #print(msg)
+        self.assertEqual(stu.uploaderror_remind(),"全部学生账号添加成功",msg)
+        print("上传文件格式为.xlsx测试成功！")
+
+        #获取页面截图
+        imagetest = getResultImage()
+        imagetest.insert_image(self.driver,"upload_format_xlsx.jpg")
+    '''
+    def test_stuaccountname_blank_run(self):
+        #账户包含空格
+        #登录系统
+        login=UserVer(self.driver)
+        login.userLogin("collegeuser","f")
+        sleep(2)
+
+        stu=StudentAccount(self.driver)
+        stu.in_batchstudent()
+        sleep(2)
+        stu.choose_existBatch()
+        stu.expirydate()
+        sleep(2)
+
+        stu.checkcount_input(checkcount=1)
+        sleep(2)
+        stu.upload_stuaccount()
+        sleep(3)
+
+        #调用autoIt生成的上传文件应用
+        stu.uploadFile_para("chrome",stu.getFilePath("学生账户1.xlsx"))
+        sleep(5)
+        msg=stu.uploaderror_remind()
+        #print(msg)
+        self.assertEqual(stu.uploaderror_remind(),"全部学生账号添加成功",msg)
+        print("上传文件格式为.xlsx测试成功！")
+'''
+
+    def test_stuAccountName_isNull_run(self):
+        '''账户为空'''
+        pass
+    def test_uploadPaper_includeNonstandard_run(self):
+        '''上传文件包含不规范账户信息'''
+        #登录系统
+        login=UserVer(self.driver)
+        login.userLogin("collegeuser","f")
+        sleep(2)
+
+        stu=StudentAccount(self.driver)
+        stu.in_batchstudent()
+        sleep(2)
+        stu.choose_existBatch()
+        stu.expirydate()
+        sleep(2)
+
+        stu.checkcount_input(checkcount=1)
+        sleep(2)
+        stu.upload_stuaccount()
+        sleep(3)
+
+        #调用autoIt生成的上传文件应用
+        stu.uploadFile_para("chrome",stu.getFilePath("学生账户3-包含错误数据.xlsx"))
+        sleep(5)
+        msg=stu.uploaderror_remind()
+        #print(msg)
+        self.assertEqual(stu.uploaderror_remind(),"全部学生账号信息不规范或该账号已存在",msg)
+        print("上传学生账号已存在测试成功！")
+
+        #获取页面截图
+        imagetest = getResultImage()
+        imagetest.insert_image(self.driver,"uploadPaper_includeNonstandard.jpg")
+
+    def test_uploadPaper_allNonstandard_run(self):
+        '''上传文件全是不规范账户信息'''
+        #登录系统
+        login=UserVer(self.driver)
+        login.userLogin("collegeuser","f")
+        sleep(2)
+
+        stu=StudentAccount(self.driver)
+        stu.in_batchstudent()
+        sleep(2)
+        stu.choose_existBatch()
+        stu.expirydate()
+        sleep(2)
+
+        stu.checkcount_input(checkcount=1)
+        sleep(2)
+        stu.upload_stuaccount()
+        sleep(3)
+
+        #调用autoIt生成的上传文件应用
+        stu.uploadFile_para("chrome",stu.getFilePath("学生账户2-全是错误数据.xlsx"))
+        sleep(5)
+        msg=stu.uploaderror_remind()
+        #print(msg)
+        self.assertEqual(stu.uploaderror_remind(),"学生账户2-全是错误数据.xlsx文件解析失败",msg)
+        print("上传文件全是不规范账户信息测试成功！")
+
+        #获取页面截图
+        imagetest = getResultImage()
+        imagetest.insert_image(self.driver,"uploadPaper_allNonstandard.jpg")
+
+
+    def test_upload_stuaccountexist_run(self):
+        '''上传的学生账户已存在'''
+        #登录系统
+        login=UserVer(self.driver)
+        login.userLogin("collegeuser","f")
+        sleep(2)
+
+        stu=StudentAccount(self.driver)
+        stu.in_batchstudent()
+        sleep(2)
+        stu.choose_existBatch()
+        stu.expirydate()
+        sleep(2)
+
+        stu.checkcount_input(checkcount=1)
+        sleep(2)
         stu.upload_stuaccount()
         sleep(3)
 
         #调用autoIt生成的上传文件应用
         stu.uploadFile_para("chrome",stu.getFilePath("111.xlsx"))
         sleep(5)
-        #msg=stu.getWinAlert()
+        msg=stu.uploaderror_remind()
         #print(msg)
-        self.assertEqual(stu.uploaderror_remind(),"全部学生账号信息不规范或该账号已存在")
-        print("上传成功")
-
+        self.assertEqual(stu.uploaderror_remind(),"全部学生账号信息不规范或该账号已存在",msg)
+        print("上传学生账号已存在测试成功！")
 
         #获取页面截图
         imagetest = getResultImage()
         imagetest.insert_image(self.driver,"uploadPaperExist.jpg")
+    def test_upload_success_run(self):
+        '''上传成功'''
+        #登录系统
+        login=UserVer(self.driver)
+        login.userLogin("collegeuser","f")
+        sleep(2)
 
+        stu=StudentAccount(self.driver)
+        stu.in_batchstudent()
+        sleep(2)
+        stu.choose_existBatch()
+        stu.expirydate()
+        sleep(2)
+
+        stu.checkcount_input(checkcount=1)
+        sleep(2)
+        stu.upload_stuaccount()
+        sleep(3)
+
+        #调用autoIt生成的上传文件应用
+        stu.uploadFile_para("chrome",stu.getFilePath("学生账户-规范的文件.xlsx"))
+        sleep(5)
+        #msg=stu.getWinAlert()
+        #print(msg)
+        self.assertEqual(stu.uploaderror_remind(),"全部学生账号添加成功")
+        print("上传学生账号已存在测试成功！")
+
+        #获取页面截图
+        imagetest = getResultImage()
+        imagetest.insert_image(self.driver,"upload_success.jpg")
 
 
 
